55cab16143715f5ed90a351d82d8ab8b1d2d4d17
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2023-05-22 03:27:48 +0300

    I corrected code

--- my-app/src/assets/hooks/hooks.js
+++ my-app/src/assets/hooks/hooks.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useParams } from 'react-router';
+import { getVacancy } from '../functions/functions';
 
 export function useDisplay(display = false) {
   const [active, setActive] = useState(display);
@@ -6,4 +8,13 @@ export function useDisplay(display = false) {
     setActive(!active);
   };
   return [active, handleActive];
+}
+
+export function useVacancy() {
+  const {id}=useParams();
+  const [vacancy,setVacancy]=useState(false)
+  const handleVacancy = async() => {
+    setVacancy(await getVacancy(id))
+  };
+  return [vacancy, handleVacancy];
 }
--- my-app/src/components/VacancyPage/VacancyPage.js
+++ my-app/src/components/VacancyPage/VacancyPage.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { useVacancy } from '../../assets/hooks/hooks';
+import { pasrePayments } from '../../assets/functions/functions';
+import star from '../../assets/images/star.svg'
+import point from '../../assets/images/point.svg'
+import location from '../../assets/images/location.svg'
+import './VacanciesPage.css'
 
 const VacancyPage = () => {
-  return <div className="vacancy"></div>;
+  const [vacancy,setVacancy]=useVacancy();
+
+  useEffect(()=>{
+    setVacancy();
+  },[])
+
+  return vacancy&&<div className="vacancy">
+     <div>
+            <p>
+              <span onClick={() => navigate(`/${vacancy.id}`)}>{vacancy.profession}</span>{' '}
+              <img src={star} />
+            </p>
+            <div>
+              <span>з/п {pasrePayments(vacancy.payment_from, vacancy.payment_to, vacancy.currency)}</span>
+              <img src={point} />
+              <p>{vacancy.type_of_work.title}</p>
+            </div>
+            <span>
+              <img src={location} />
+              <p>{vacancy.town.title}</p>
+            </span>
+          </div>
+  </div>;
 };
 
 export default VacancyPage;
