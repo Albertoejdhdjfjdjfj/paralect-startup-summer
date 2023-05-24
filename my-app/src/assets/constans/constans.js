b4158429c00bc617fe747bd4a921c54c64f90e2a
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2023-05-24 23:51:24 +0300

    I corrected code

--- my-app/src/assets/constans/constans.js
+++ my-app/src/assets/constans/constans.js
@@ -9,4 +9,4 @@ export const x_secret_key = 'GEU4nvd3rej*jeh.eqp';
 export const x_api_app_id =
   'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948';
 export const num_vacancies = 4;
-export const num_pages = 125;
+export const num_pages = 500;
